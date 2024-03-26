daa287e7d1f6bd37aefef8040a36bcaf38f59876
Bhekisile <bhekisilejozi@gmail.com>
2024-03-26 14:18:17 +0200

    Display the new friend form

--- src/App.js
+++ src/App.js
@@ -26,8 +26,11 @@ function App() {
     <div className="app">
       <div className="sidebar">
         <FriendsList />
+        <Button>Add friend</Button>
         <FormAddFriend />
       </div>
+
+      <FormSplitBill />
     </div>
   );
 }
@@ -110,6 +113,31 @@ function FormAddFriend() {
 
       <Button>Add</Button>
 
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+
+      <label>💰 Bill value</label>
+      <input type="text" />
+
+      <label>🕴️ Your expense</label>
+      <input type="text" />
+
+      <label>🧑‍🤝‍🧑 X's expense</label>
+      <input type="text" disabled />
+
+      <label>🧑 Who is paying the bill</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <Button>Split bill</Button>
     </form>
   );
 }
